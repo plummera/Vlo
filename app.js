0859553c7270027cef67ed6d86e27817e31c7444
Anthony Plummer <aplummer@psda.com>
2018-12-04 18:39:28 -0500

    no message

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
 var app = angular.module("vlocityApp", []);
-
-var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
-  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || 'myMasterKey'
-});
-
-// Serve the Parse API on the /parse URL prefix
-var mountPath = process.env.PARSE_MOUNT || '/parse';
